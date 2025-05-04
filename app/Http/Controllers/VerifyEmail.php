bfd93060a0505d38ff5c47881d34ad5a2e285e29
Nvtoan02myjob <nvtoan29102002@gmail.com>
2025-05-05 00:47:37 +0700

    busy

--- app/Http/Controllers/VerifyEmail.php
+++ app/Http/Controllers/VerifyEmail.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\EmailVerifications;
+use App\Models\emailVerifications;
 use App\Models\User;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Hash;
